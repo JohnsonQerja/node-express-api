1cfb662acced3f7a31c5ff3f1d0ea721f65254ae
johnson286 <johnson.lin.286@gmail.com>
2023-01-08 20:21:25 +0700

    commit fix db config

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const likeRoute = require('./api/routes/like');
 const commentRoute = require('./api/routes/comment');
 
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.DB_URL_DEV, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('DB Connected!'));
